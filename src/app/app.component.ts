9a0dc900fb0afc2c66014e92dfe25599dc6ebf7a
Greg Doud <gpdoud@gmail.com>
2021-08-06 13:22:51 -0400

    Committed by GitUtil

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Friend } from './friend.class';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'friends-list';
+
+  friends: Friend[] = [
+    new Friend("Alan", 33, "alan@friend.com", false),
+    new Friend("Barb", 34, "barb@friend.com", false),
+    new Friend("Chris", 35, "chris@friend.com", true),
+    new Friend("Darlene", 36, "darlene@friend.com", true),
+    new Friend("Ed", 37, "ed@friend.com", false)
+  ];
+  btnColor: string = "btn btn-danger";
+
 }
